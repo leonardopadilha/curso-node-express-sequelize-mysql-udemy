c4841dac8ed9a3e025e8e30557778177e5528ee4
leonardopadilha <leonardopadilha28@gmail.com>
2023-02-18 10:57:16 -0300

    Relacionamento entre tabelas ( 1 - n)

---
+++ models/endereco.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Endereco extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Endereco.belongsTo(models.Pessoa, { // Está com a chave estrangeira - Pertence a
+        foreignKey: 'pessoaId'
+      })
+    }
+  }
+  Endereco.init({
+    estado: DataTypes.STRING,
+    cidade: DataTypes.STRING,
+    bairro: DataTypes.STRING,
+    rua: DataTypes.STRING,
+    numero: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Endereco',
+  });
+  return Endereco;
+};
