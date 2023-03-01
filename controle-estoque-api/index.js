4a7d0500055835cfc6604bb96f281da983bc1cfd
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-01 05:09:43 -0300

    Configurando api controle de estoque

---
+++ controle-estoque-api/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const itemRoute = require('./src/routes/item.route');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/itens', itemRoute);
+
+app.listen(process.env.PORT, () => { console.log('rodando') })
