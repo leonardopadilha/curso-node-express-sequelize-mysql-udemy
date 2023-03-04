ea3b25d16f34af04227e7f4863f720c9485fbb45
leonardopadilha <leonardopadilha28@gmail.com>
2023-03-04 19:59:24 -0300

    Middleware - Verificando se usuário está logado

--- controle-estoque-api/src/routes/usuario.route.js
+++ controle-estoque-api/src/routes/usuario.route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
 const usuarioValidator = require('../validators/usuario.validator');
+const verifyJWT = require('../middlewares/authorizator');
 
 router
     .post('/', usuarioValidator.create(), usuarioController.create)
     .post('/login', usuarioValidator.login(), usuarioController.login)
-    .put('/:id', usuarioValidator.updateUser(), usuarioController.updateUser)
-    .delete('/:id', usuarioValidator.deleteUser(), usuarioController.deleteUser)
-    .get('/', usuarioController.findUsers)
-    .get('/:id', usuarioValidator.findUserById(), usuarioController.findUserById)
+    .put('/:id', verifyJWT, usuarioValidator.updateUser(), usuarioController.updateUser)
+    .delete('/:id', verifyJWT, usuarioValidator.deleteUser(), usuarioController.deleteUser)
+    .get('/', verifyJWT, usuarioController.findUsers)
+    .get('/:id', verifyJWT, usuarioValidator.findUserById(), usuarioController.findUserById)
 
 module.exports = router;
